f3e00307ca92a2cfb02e04a69aeb251dfd8ccc3d
Abhirashmi <65527504+Abhirashmi@users.noreply.github.com>
2021-01-30 10:01:57 +0530

    Add files via upload

---
+++ 00_basic.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tuts</title>
+</head>
+<body>
+    <div class="container">
+    <h1>php tuts</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, odit quasi debitis reiciendis adipisci laudantium aperiam est quia, culpa repellat natus error id animi consectetur quidem. Deserunt unde vel voluptatem quidem tempora ducimus vitae animi amet assumenda dolorum, natus earum praesentium numquam illo libero exercitationem perspiciatis officia magnam ut quae, necessitatibus blanditiis dolorem! Voluptatem eveniet id incidunt natus nesciunt quod velit reiciendis pariatur asperiores saepe deleniti, numquam non voluptatum totam iure nostrum labore sint? Minima placeat dolores reiciendis ipsa, nemo quo laboriosam magnam optio recusandae necessitatibus voluptate at sit excepturi perspiciatis expedita facere molestias ab itaque quae soluta et!</p>
+    this is my first php website </div>
+    <?php
+
+    echo "this is printed using php";
+    ?>
+</body>
+</html>
